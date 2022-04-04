78bab0f98ab351bdec217be9f8349bc87744067c
James Yeap <jamesyeap.ys@gmail.com>
2022-04-04 20:57:27 +0800

    Remove edits to Function 3

--- proc.sql
+++ proc.sql
@@ -809,16 +809,16 @@ RETURN QUERY (
             SELECT DISTINCT S.id, 
                 (
                     /* scalar query to get number of refund requests per shop */
-                    COALESCE((SELECT COUNT(*) FROM no_of_refund_requests R WHERE R.shop_id = S.id), 0) + 
+                    (SELECT COUNT(*) FROM no_of_refund_requests R WHERE R.shop_id = S.id) + 
 
                     /* scalar query to get number of shop complaints per shop */
-                    COALESCE((SELECT COUNT(*) FROM shop_complaint SC WHERE SC.shop_id = S.id), 0) + 
+                    (SELECT COUNT(*) FROM shop_complaint SC WHERE SC.shop_id = S.id) + 
 
                     /* scalar query to get number of delivery complaints per shop */
-                    COALESCE((SELECT COUNT(*) FROM no_of_delivery_complaints DC WHERE DC.shop_id = S.id), 0) + 
+                    (SELECT COUNT(*) FROM no_of_delivery_complaints DC WHERE DC.shop_id = S.id) + 
 
                     /* scalar query to get number of 1-star reviews per shop */
-                    COALESCE((SELECT COUNT(*) FROM latest_review_version_to_shop RV WHERE RV.shop_id = S.id), 0)
+                    (SELECT COUNT(*) FROM latest_review_version_to_shop RV WHERE RV.shop_id = S.id)
                 ) :: INTEGER AS num_negative_indicators_per_shop
 
             FROM shop S   
